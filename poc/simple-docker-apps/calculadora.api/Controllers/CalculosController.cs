4a0117d56c55242b75448a9f101e9e5a21b8d86c
Tiago Felipe dos Santos <tiago.santos@CONTMATIC.AD>
2017-11-09 14:18:26 -0200

    criação das APIs de testes

---
+++ poc/simple-docker-apps/calculadora.api/Controllers/CalculosController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace calculadora.api.Controllers
+{
+    [Route("api/calculos")]
+    public class CalculosController : Controller
+    {
+        [HttpGet("somar/{a:int}/{b:int}")]
+        public IActionResult Somar(int a, int b)
+        {
+            return Ok(a + b);
+        }
+
+        [HttpGet("subtrair/{a:int}/{b:int}")]
+        public IActionResult Subtrair(int a, int b)
+        {
+            return Ok(a - b);
+        }
+    }
+}
